67434e4474ee26cc7c711bb7f06c21e75f064249
cate <cate.boehm@oebb.at>
2025-05-19 11:14:45 +0200

    mvvm setup gemacht und es startet

--- Forum/App.xaml.cs
+++ Forum/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Forum.Models;
+using Forum.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +13,27 @@ namespace Forum
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        public App()
+        {
+            ServiceCollection builder = new ServiceCollection();
+
+            builder.AddDbContext<ForumContext>(options =>
+            options.UseSqlServer("Data Source=CATE_;Initial Catalog=Forum;Integrated Security=True;TrustServerCertificate=True"));
+
+            builder.AddTransient<MainWindow>();
+            builder.AddTransient<MainViewModel>();
+
+            ServiceProvider = builder.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = ServiceProvider.GetRequiredService<MainWindow>();
+            window.Show();
+        }
     }
 
 }
